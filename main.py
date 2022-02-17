09f40e0dc1d26780d0cadc9d3573595db1c00a6a
avonderg <77714730+avonderg@users.noreply.github.com>
2022-02-16 21:38:35 -0500

    done

--- main.py
+++ main.py
@@ -32,6 +32,7 @@ CENSUS_FILE_PATH = ROOT_DIR_PREFIX + DATA_FILE_NAME
 
 NUM_CLASSES = 3
 BATCH_SIZE = 60  #tune this parameter
+# BATCH_SIZE = 99  #tune this parameter
 CONV_THRESHOLD = 0.0001 #tune this parameter
 
 def import_census(file_path):
